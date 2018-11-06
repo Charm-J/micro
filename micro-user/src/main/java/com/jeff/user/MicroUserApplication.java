90108dc97808c32784dea34b327bbd4e489b5af8
DJ <DJ@192.168.1.108>
2018-11-06 18:51:35 +0800

    t

--- micro-user/src/main/java/com/jeff/user/MicroUserApplication.java
+++ micro-user/src/main/java/com/jeff/user/MicroUserApplication.java
@@ -1,6 +1,5 @@
 package com.jeff.user;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,7 +8,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@EnableDubboConfiguration
 @EnableTransactionManagement
 // 此处要特别注意-MapperScan引入的是tk.mybatis.spring.annotation.MapperScan而不是org.mybatis.spring.annotation.MapperScan
 @MapperScan(basePackages = "com.jeff.user.dao")
